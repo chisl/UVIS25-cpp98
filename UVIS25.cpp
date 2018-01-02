31dba651747e0645d1c6a61fc95d17bca07085cc
chisl <info@chisl.io>
2018-01-02 16:25:40 +0100

    Initial commit

---
+++ UVIS25.cpp
@@ -0,0 +1,18 @@
+/*
+ * name:        UVIS25
+ * description: Digital UV index sensor: 0 - 15 UV index output range.
+ * manuf:       STMicroelectronics
+ * version:     0.1
+ * url:         http://www.st.com/resource/en/datasheet/uvis25.pdf
+ * date:        2018-01-02
+ * author       https://chisl.io/
+ * file:        UVIS25.cpp
+ */
+
+/*                                                                                                       *
+ *                                   THIS FILE IS AUTOMATICALLY CREATED                                  *
+ *                                    D O     N O T     M O D I F Y  !                                   *
+ *                                                                                                       */
+
+#include "UVIS25.hpp"
+
